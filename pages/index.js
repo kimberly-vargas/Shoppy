2aa2dccf856d0fab3455730d80de9281e06429da
kimberly-vargas <vargas.kim.d@estudiantec.cr>
2022-11-17 12:01:35 -0600

    conection with Sanity

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,35 @@
 import React from "react";
+import { client } from "../Lib/client";
 import { Product, FooterBanner, HeroBanner } from "../components";
 
-const Home = () => {
+const Home = ({ products, bannerData }) => {
   return (
     <div>
-      <HeroBanner />
+      <HeroBanner heroBanner={bannerData.length && bannerData[0]}/>
       <div className="products-heading">
         <h2>Best Seller Products</h2>
         <p>speaker There are many variations passages</p>
       </div>
 
       <div className="products-container">
-        {['p1', 'p2', 'p3'].map((product) => (
-          product
-        ))}
+        {products?.map((product) => product.name)}
       </div>
 
       <FooterBanner/>
     </div>
   );
 };
+
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: { products, bannerData }
+  }
+}
 
 export default Home;
